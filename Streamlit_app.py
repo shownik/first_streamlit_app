3eddee8e760ade1bfc15952db2572aae1be80ed4
shownik <54351510+shownik@users.noreply.github.com>
2024-02-05 16:14:36 +0530

    Update Streamlit_app.py
    Change header

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
