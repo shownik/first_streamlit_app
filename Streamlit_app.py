c9f81bc1bb35be5605a70d1ff6f72ccf11c21a71
shownik <54351510+shownik@users.noreply.github.com>
2024-02-05 16:25:48 +0530

    Update Streamlit_app.py
    Add instruction

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -4,5 +4,9 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 import pandas
+
+#Let's put a pick list here so they can pick the fruit they want to include
 my_fruit_list= pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+#display the table on the page
 streamlit.dataframe(my_fruit_list)
