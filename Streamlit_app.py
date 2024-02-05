d57a51c92dafc76a248e74cd98b33565a4e332f0
shownik <54351510+shownik@users.noreply.github.com>
2024-02-06 02:34:39 +0530

    Update Streamlit_app.py
    Delete row and add extension

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -21,8 +21,8 @@ streamlit.header('Fruityvice Fruit Advice')
 
 #new section to display fruityvice api response
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())#just writes the data to the screen
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+
 
 #take the json verson of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
